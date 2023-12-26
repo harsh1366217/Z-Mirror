3983e1d802913b43cb1d78c1718add0855367a3d
harsh1366217 <146430509+harsh1366217@users.noreply.github.com>
2023-12-26 11:48:53 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -255,7 +255,7 @@ async def start(_, message):
                        'Use me at: @Z_Mirror'
     elif not config_dict['DM_MODE'] and message.chat.type != message.chat.type.SUPERGROUP:
         start_string = 'Sorry, you cannot use me here!\n' \
-                       'Join: @Z_Mirror to use me.\n' \
+                       'Join: @public_leech_76 to use me.\n' \
                        'Thank You'
     else:
         tag = message.from_user.mention
